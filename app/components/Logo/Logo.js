b431d46794fbe02e5d09ca4962eb1a29d88487d7
Hieu Phan <hieu.phq@dwarvesf.com>
2017-10-11 13:54:24 +0700

    Add logo to Main page

---
+++ app/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import styles from './styles';
+
+const Logo = () => (
+  <View style={styles.container}>
+    <Image style={styles.containerImage} source={require('./images/background.png')}>
+      <Image resizeMode="contain" style={styles.logo} source={require('./images/logo.png')} />
+    </Image>
+    <Text style={styles.text}>Currency Converter</Text>
+  </View>
+);
+
+export default Logo;
