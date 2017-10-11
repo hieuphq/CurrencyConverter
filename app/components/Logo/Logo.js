3336b1e268a0cf6e5514c42138df8fcba133cfab
Hieu Phan <hieu.phq@dwarvesf.com>
2017-10-11 16:12:15 +0700

    Add TextInput to Home page

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -4,7 +4,11 @@ import styles from './styles';
 
 const Logo = () => (
   <View style={styles.container}>
-    <Image style={styles.containerImage} source={require('./images/background.png')}>
+    <Image
+      resizeMode="contain"
+      style={styles.containerImage}
+      source={require('./images/background.png')}
+    >
       <Image resizeMode="contain" style={styles.logo} source={require('./images/logo.png')} />
     </Image>
     <Text style={styles.text}>Currency Converter</Text>
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
+import { InputWithButton } from '../components/TextInput';
 
-export default () => (
-  <Container>
-    <StatusBar translucent={false} barStyle="default" />
-    <Logo />
-  </Container>
-);
+const TEMP_BASE_CURRENCY = 'USD';
+const TEMP_QUOTE_CURRENCY = 'GBP';
+const TEMP_BASE_PRICE = '100';
+const TEMP_QUOTE_PRICE = '69.96';
+
+class Home extends Component {
+  handlePressBaseCurrency = () => {
+    console.log('press base');
+  };
+
+  handlePressQuoteCurrency = () => {
+    console.log('press quote');
+  };
+
+  handleTextChange = (text) => {
+    console.log(`Changed text ${text}`);
+  };
+
+  render() {
+    return (
+      <Container>
+        <StatusBar translucent={false} barStyle="default" />
+        <Logo />
+        <InputWithButton
+          buttonText={TEMP_BASE_CURRENCY}
+          onPress={this.handlePressBaseCurrency}
+          defaultValue={TEMP_BASE_PRICE}
+          keyboardType="numeric"
+          onChangeText={this.handleTextChange}
+        />
+        <InputWithButton
+          buttonText={TEMP_QUOTE_CURRENCY}
+          onPress={this.handlePressQuoteCurrency}
+          editable={false}
+          value={TEMP_QUOTE_PRICE}
+        />
+      </Container>
+    );
+  }
+}
+export default Home;
