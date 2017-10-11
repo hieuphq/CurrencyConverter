564508685811a021ce03c387c5c7da0e5297ca83
Hieu Phan <hieu.phq@dwarvesf.com>
2017-10-12 00:04:14 +0700

    Add Currency List page

---
+++ app/components/List/Icon.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, Image } from 'react-native';
+
+import styles from './styles';
+
+const Icon = ({ checkmark, visible }) => {
+  if (visible) {
+    const iconStyles = [styles.icon];
+    if (visible) {
+      iconStyles.push(styles.iconVisible);
+    }
+
+    return (
+      <View style={iconStyles}>
+        {checkmark ? (
+          <Image
+            source={require('./images/check.png')}
+            style={styles.checkIcon}
+            resizeMode="contain"
+          />
+        ) : null}
+      </View>
+    );
+  }
+
+  return <View style={styles.icon} />;
+};
+
+Icon.propTypes = {
+  checkmark: PropTypes.bool,
+  visible: PropTypes.bool,
+};
+
+export default Icon;
