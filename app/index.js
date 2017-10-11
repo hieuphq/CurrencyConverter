ef1a4dfafb15b78fe60bc9fe68e68ca4384d5f43
Hieu Phan <hieu.phq@dwarvesf.com>
2017-10-12 00:57:52 +0700

    Themes Screen

--- app/index.js
+++ app/index.js
@@ -7,10 +7,14 @@ import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 // import Home from './screens/Home';
 // import CurrencyList from './screens/CurrencyList';
-import Options from './screens/Options';
+// import Options from './screens/Options';
+import Themes from './screens/Themes';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
+  $primaryOrange: '#D57A66',
+  $primaryGreen: '#00BD9D',
+  $primaryPurple: '#9E768F',
   $white: '#fff',
   $border: '#E2E2E2',
   $inputText: '#797979',
@@ -19,4 +23,4 @@ EStyleSheet.build({
   // $outline: 1,
 });
 
-export default () => <Options />;
+export default () => <Themes />;
