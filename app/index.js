34af3854098cd5dcbf13f9ee6ec57f8d2e34233c
Hieu Phan <hieu.phq@dwarvesf.com>
2017-10-13 14:56:39 +0700

    Alert component

--- app/index.js
+++ app/index.js
@@ -5,9 +5,8 @@
  */
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
-// import Home from './screens/Home';
-// import CurrencyList from './screens/CurrencyList';
-// import Options from './screens/Options';
+
+import { AlertProvider } from './components/Alert';
 import Navigator from './config/routes';
 
 EStyleSheet.build({
@@ -23,4 +22,8 @@ EStyleSheet.build({
   // $outline: 1,
 });
 
-export default () => <Navigator />;
+export default () => (
+  <AlertProvider>
+    <Navigator />
+  </AlertProvider>
+);
--- app/screens/Options.js
+++ app/screens/Options.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { ScrollView, StatusBar, Platform } from 'react-native';
+import { ScrollView, StatusBar, Platform, Linking } from 'react-native';
 import PropTypes from 'prop-types';
 
 import { ListItem, Separator } from '../components/List';
+import { connectAlert } from '../components/Alert';
 
 const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
 const ICON_COLOR = '#868686';
@@ -11,13 +12,17 @@ const ICON_SIZE = 23;
 class Options extends Component {
   static propTypes = {
     navigation: PropTypes.object,
+    alertWithType: PropTypes.func,
   };
 
   handleThemesPress = () => {
     this.props.navigation.navigate('Themes');
   };
 
-  handleSitePress = () => {};
+  handleSitePress = () => {
+    Linking.openURL('http://fixer.io').catch(() =>
+      this.props.alertWithType('error', 'Sorry!', "Fixer.io can't be open right now"));
+  };
 
   render() {
     return (
@@ -31,4 +36,4 @@ class Options extends Component {
   }
 }
 
-export default Options;
+export default connectAlert(Options);
